22b613f4696287001133cb26d0ad88d4855fa6be
Winteler <nicolas.winteler@bluewin.ch>
2017-05-11 15:08:51 +0200

    nouveau metrics

--- GMR_comparison/GMM_reg.m
+++ GMR_comparison/GMM_reg.m
@@ -1,25 +1,32 @@
 clc
 clearvars
 close all
+data_source = 1;
 
+n_loop = 1;
+time = zeros(n_loop,1);
+%error = zeros(n_loop,1);
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                               LOAD DATASET                              % 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-addpath('matlab_function');
-addpath('Datasets');
-addpath(genpath('ML_toolbox-master'));
+if data_source == 1
+    addpath('matlab_function');
+    addpath('Datasets');
+    addpath(genpath('ML_toolbox-master'));
 
-%load('2D-GMM.mat')
-load('GMR_data.mat')
+    %load('2D-GMM.mat')
+    load('RedWine_Quality.mat')
+    if (size(X,1)<4)
+        % Visualize Dataset
+        options.class_names = {};
+        options.title       = '2D Concentric Circles Dataset';
+        %options.labels       = y;
 
-% Visualize Dataset
-options.class_names = {};
-options.title       = '2D Concentric Circles Dataset';
-%options.labels       = y;
-
-if exist('h0','var') && isvalid(h0), delete(h0);end
-h0 = ml_plot_data(X',options);hold on;
+        if exist('h0','var') && isvalid(h0), delete(h0);end
+        h0 = ml_plot_data(X',options);hold on;
+    end
+end
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                               LOAD DATASET                              % 
@@ -60,18 +67,20 @@ plot_mixture(X_train, ones(1,size(X_train,2)))
 K_range=[1:10]; cov_type = 'full'; repeats = 10;
 
 % Evaluation of gmm-em in order to find the optimal k
-gmm_eval(X, K_range, repeats, cov_type);
-
+for l = 1:n_loop
+    tic;
+    gmm_eval(X, K_range, repeats, cov_type);
+    time(l) = toc;
+end
+mean(time)
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                 Choice of the GMM-hyperparameters                       %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % From the previous graph choose the best value of K
-K = 4; cov_type = 'full';  plot_iter = 0;
+K = 8; cov_type = 'full';  plot_iter = 0;
 
 % Run MY GMM-EM function, estimates the paramaters by maximizing loglik
-tic;
 [Priors, Mu, Sigma] = ml_gmmEM(X, K);
-toc;
 
 % Visualize GMM pdf from learnt parameters
 close all;
@@ -82,7 +91,15 @@ ml_plot_gmm_pdf(X, Priors, Mu, Sigma)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 in = 1; out = 2;
 
-x = linspace(-30,40,300);
+%x = linspace(-30,40,300);
+x = X(1:end-1,:);
 [y_est, Sigma_y] = ml_gmr(Priors, Mu, Sigma, x, in, out);
+[y_est_test, Sigma_y_test] = ml_gmr(Priors, Mu, Sigma, X_test(1:end-1,:), in, out);
+
+if data_source == 0
+    error = var(y_est_test'-y_test)
+else
+    error = var(y_est_test-X_test(end,:))
+end
 
 ml_plot_gmr_function(x', y_est, Sigma_y,'var_scale');
--- GMR_comparison/GMR_comparison.m
+++ GMR_comparison/GMR_comparison.m
@@ -2,6 +2,12 @@ clc
 clearvars
 close all
 data_source = 0;
+n_loop = 5;
+n_iter = 5;
+time = zeros(n_loop,1);
+MSE = zeros(n_loop,1);NMSE = zeros(n_loop,1);Rsquared = zeros(n_loop,1);
+a_0 = 50;
+b_0 = 0.01;
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                               LOAD DATASET                              % 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -11,15 +17,16 @@ if data_source == 1
     addpath(genpath('ML_toolbox-master'));
 
     %load('2D-GMM.mat')
-    load('2D-GMM.mat')
+    load('RedWine_Quality.mat')
+    if (size(X,1)<4)
+        % Visualize Dataset
+        options.class_names = {};
+        options.title       = '2D Concentric Circles Dataset';
+        %options.labels       = y;
 
-    % Visualize Dataset
-    options.class_names = {};
-    options.title       = '2D Concentric Circles Dataset';
-    %options.labels       = y;
-
-    if exist('h0','var') && isvalid(h0), delete(h0);end
-    h0 = ml_plot_data(X',options);hold on;
+        if exist('h0','var') && isvalid(h0), delete(h0);end
+        h0 = ml_plot_data(X',options);hold on;
+    end
 end
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -53,58 +60,77 @@ plot_mixture(X_train, ones(1,size(X_train,2)))
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                 DP_GMM                                  % 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% run the CRP sampler to generate the posterior distribution over model 
-% parameters
-tic;
-[class_id, mean_record, covariance_record, K_record, lP_record, alpha_record] = sampler(X_train, 200,10);
-
-%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%                             DP_GMR Gaussians                            % 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-Priors = zeros(1, K_record(end)-1);
-Mu = zeros(data_size(1), K_record(end)-1);
-Sigma = zeros(data_size(1),data_size(1), K_record(end)-1);
-Mu_DP = cell2mat(mean_record(end));
-Sigma_DP = cell2mat(covariance_record(end));
-for i = 1:K_record(end)-1
-    class = class_id(:,end);
-    X_k = X_train(:,class == i);
-    X_k_c = zeros(data_size(1),size(X_k,2));
-    [i,length(X_k)]
-    Priors(i) = size(X_k,2)/size(X_train,2);
-    Mu(:,i) = mean(X_k,2);
-    if (size(X_k,2) > 2);
-        for j = 1:data_size(1)
-            X_k_c(j,:) = X_k(j,:) - Mu(j,i);
+lP_max = -1000000;
+for l = 1:n_loop
+    %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %                                 DP_GMM                                  % 
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    tic;
+    for k = 1:n_iter
+        % run the CRP sampler to generate the posterior distribution over model 
+        % parameters
+        [class_id, mean_record, covariance_record, K_record, lP_record, alpha_record] = sampler(X_train, 200,a_0,b_0);
+        
+        [max_p, pos] = max(lP_record);
+        if (max_p > lP_max)
+            id_best = class_id(:,pos);
+            mean_best = mean_record(pos);
+            covar_best = covariance_record(pos);
+            k_best = K_record(pos)-1;
         end
-        Sigma(:,:,i) = X_k_c*X_k_c'/(size(X_k,2)-1);
+    end
+    %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %                             DP_GMR Gaussians                            % 
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    Priors = zeros(1, k_best);
+    Mu = zeros(data_size(1), k_best);
+    Sigma = zeros(data_size(1),data_size(1), k_best);
+    Mu_DP = cell2mat(mean_best);
+    Sigma_DP = cell2mat(covar_best);
+    for i = 1:k_best
+        class = id_best;
+        X_k = X_train(:,class == i);
+        X_k_c = zeros(data_size(1),size(X_k,2));
+        Priors(i) = size(X_k,2)/size(X_train,2);
+        Mu(:,i) = mean(X_k,2);
+        if (size(X_k,2) > data_size(1));
+            for j = 1:data_size(1)
+                X_k_c(j,:) = X_k(j,:) - Mu(j,i);
+            end
+            Sigma(:,:,i) = X_k_c*X_k_c'/(size(X_k,2)-1);
+        else
+            Sigma(:,:,i) = Sigma_DP(:,:,i);
+        end
+    end
+
+    time(l) = toc;
+    if (data_size(1) == 2)
+        ml_plot_gmm_pdf(X, Priors, Mu, Sigma)
+    end
+
+    %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %                           Regression                                    %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    in = 1: data_size(1)-1; 
+    out = data_size(1);
+
+    %x = linspace(min(X(1,:)),max(X(1,:)),300);
+    x = X(1:end-1,:);
+    [y_est, Sigma_y] = ml_gmr(Priors, Mu, Sigma, x, in, out);
+    
+    [y_est_test, Sigma_y_test] = ml_gmr(Priors, Mu, Sigma, X_test(1:end-1,:), in, out);
+
+    if data_source == 0
+        [MSE(l), NMSE(l), Rsquared(l)] = my_regression_metrics( y_est_test, y_test');
     else
-        Sigma(:,:,i) = Sigma_DP(:,:,i);
+        [MSE(l), NMSE(l), Rsquared(l)] = my_regression_metrics( y_est_test', X_test(end,:) );
+    end
+    if (size(X,1)<3)
+        ml_plot_gmr_function(x', y_est, Sigma_y,'var_scale');
     end
 end
-
-time = toc
-if (data_size(1) == 2)
-    ml_plot_gmm_pdf(X, Priors, Mu, Sigma)
-end
-
-%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%                           Regression                                    %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-in = 1: data_size(1)-1; 
-out = data_size(1);
-
-%x = linspace(min(X(1,:)),max(X(1,:)),300);
-x = X(1:end-1,:);
-[y_est, Sigma_y] = ml_gmr(Priors, Mu, Sigma, x, in, out);
-
-[y_est_test, Sigma_y_test] = ml_gmr(Priors, Mu, Sigma, X_test(1:end-1,:), in, out);
-
-if data_source == 0
-    error = var(y_est_test'-y_test)
-else
-    error = var(y_est_test'-X_test(end,:))
-end
-ml_plot_gmr_function(x', y_est, Sigma_y,'var_scale');
+avg_time = mean(time)/n_iter
+avg_mse = mean(MSE)
+avg_nmse = mean(NMSE)
+avg_rsquared = mean(Rsquared)
